40c7a22e9ec42fe5d8c5215175be8b1b25f65e09
zakaria <banidzakaria@gmail.com>
2024-02-15 10:49:52 +0100

    first commit

---
+++ src/com/company/Collector.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Collector {
+    String address;
+    String name;
+    int dollNumber;
+
+    //ADD DOLL FUNCTION
+    void addDoll(){
+
+    }
+
+
+    //DELETE DOLL FUNCTION
+    void deleteDoll(){
+
+    }
+
+
+
+
+}
---
+++ src/com/company/russianDoll.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class russianDoll {
+    int size;
+    String color;
+    int fabricationDate;
+}
